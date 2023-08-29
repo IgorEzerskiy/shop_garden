6cf0e2cfccfc2708a56427944f7982a223865a0d
Dankir30 <kirrdann@gmail.com>
2023-08-29 22:35:01 +0300

    admin pages and fixed models

--- shop_main_app/admin.py
+++ shop_main_app/admin.py
@@ -1,7 +1,29 @@
 from django.contrib import admin
-from shop_main_app.models import Category, Measure, Product
+from shop_main_app.models import Category, Measure, Product, ProductImage
+
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(Measure)
-admin.site.register(Product)
+
+
+# admin.site.register(Product)
+
+
+class ProductImageAdmin(admin.StackedInline):
+    model = ProductImage
+
+
+@admin.register(Product)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [ProductImageAdmin]
+    list_display = ['title', 'price', 'quantity', 'availability']
+
+
+    class Meta:
+        model = Product
+
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    pass
--- shop_main_app/models.py
+++ shop_main_app/models.py
@@ -5,32 +5,46 @@ from django.db import models
 
 
 class Measure(models.Model):
-    name = models.CharField()
+    name = models.CharField(verbose_name='Назва')
+
+    def __str__(self):
+        return self.name
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, verbose_name='Назва')
     slug = models.SlugField()
-    description = models.TextField(max_length=550)
+    description = models.TextField(max_length=550, verbose_name='Опис')
     image = models.ImageField(
         upload_to='shop_garden/media/category_images/',
-        max_length=100
+        max_length=100,
+        verbose_name='Зображення'
     )
+
+    def __str__(self):
+        return self.name
 
 
 class Product(models.Model):
-    title = models.CharField(max_length=256)
+    title = models.CharField(max_length=256, verbose_name='Назва')
     slug = models.SlugField()
-    description = models.TextField()
-    characteristics = models.TextField()
-    quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    availability = models.BooleanField(default=True)
-    measure = models.ForeignKey(Measure, on_delete=models.DO_NOTHING, related_name='product')
+    description = models.TextField(verbose_name='Опис')
+    characteristics = models.TextField(verbose_name='Характеристики')
+    quantity = models.IntegerField(verbose_name='К-сть')
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Ціна')
+    availability = models.BooleanField(default=True, verbose_name='Наявність')
+    measure = models.ForeignKey(Measure, on_delete=models.DO_NOTHING, related_name='product', verbose_name='Міра')
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    images = models.FileField(upload_to='product_images/')
-    category = models.ManyToManyField(Category, related_name='products')
+    category = models.ManyToManyField(Category, related_name='products', verbose_name='Категорія')
     index = models.BooleanField(default=True)
-    discount = models.PositiveIntegerField(default=0)
+    discount = models.PositiveIntegerField(default=0, verbose_name='Знижка')
     supplier_discount = models.TextField(max_length=500)
+
+    def __str__(self):
+        return f'Товар {self.title}'
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product', verbose_name='Товар')
+    image = models.ImageField(upload_to='product_images/', verbose_name='Зображення')

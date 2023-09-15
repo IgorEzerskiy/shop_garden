5ba20b4221560f95bf1b4fa000d599d2fe8d7a81
Dankir30 <kirrdann@gmail.com>
2023-09-15 12:01:35 +0300

    footer admin adding and deleting permission

--- options_app/admin.py
+++ options_app/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 
 from options_app.models import Footer, Carousel
 
+
 # Register your models here.
-admin.site.register(Footer)
+
+
+@admin.register(Footer)
+class AuthorAdmin(admin.ModelAdmin):
+
+    # This will help you to disbale add functionality
+    def has_add_permission(self, request):
+        return False
+
+    # This will help you to disable delete functionality
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(Carousel)

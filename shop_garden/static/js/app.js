388498ce280645d8227a957df106844681072d85
Dankir30 <kirrdann@gmail.com>
2023-09-20 14:38:09 +0300

    fixed filter js

--- shop_garden/static/js/app.js
+++ shop_garden/static/js/app.js
@@ -13,7 +13,7 @@ let maxVal = parseInt(rangeInputs[1].value);
 
 if (full_scale === 0){
     rangeInputs[1].max = maxVal + 1
-    // rangeInputs[1].value = maxVal + 1
+    rangeInputs[1].value = maxVal + 1
     range.style.left = 0 +"%";
     range.style.right = 0 + "%";
 }
@@ -27,23 +27,30 @@ else {
 
 priceInputs.forEach((input) => {
   input.addEventListener("input", (e) => {
-    let minPrice = priceInputs[0].value === '' ? '0': (priceInputs[0].value)
-
-    console.log(minPrice)
-
-    let maxPrice = (priceInputs[1].value);
-
-    console.log(maxPrice)
+    let minPrice = priceInputs[0].value === '' ? 0: parseInt(priceInputs[0].value)
 
 
-    if (maxPrice - minPrice >= priceGap && maxPrice <= rangeInputs[1].max) {
+    let maxPrice = parseInt(priceInputs[1].value);
+
+
+
+    if ( minPrice >=rangeInputs[0].min && maxPrice - minPrice >= priceGap && maxPrice <= rangeInputs[1].max) {
       if (e.target.className === "min-input") {
         rangeInputs[0].value = minPrice;
-       range.style.left = ((100 * (minVal - rangeInputs[0].min)) / full_scale) + "%";
+        range.style.left = ((100 * (minPrice - rangeInputs[0].min)) / full_scale) + "%";
+         console.log(range.style.left)
       } else {
         rangeInputs[1].value = maxPrice;
-        range.style.right = ((100 * (rangeInputs[0].max - maxVal)) / full_scale)+ "%";
+        range.style.right = ((100 * (rangeInputs[1].max - maxPrice)) / full_scale)+ "%";
+        console.log(range.style.left)
       }
+    }
+    else {
+      rangeInputs[0].value = rangeInputs[0].min;
+      range.style.left = 0 + "%";
+      rangeInputs[1].value = rangeInputs[0].max;
+      range.style.right =  0 + "%";
+
     }
   });
 });

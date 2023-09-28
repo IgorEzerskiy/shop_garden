37dd9575f20c055ca5804ca5c24009211ca4e50b
Igor Laptop <you@example.com>
2023-09-29 00:05:21 +0300

    added order models to django admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from orders.forms import OrderItemModelForm
 
 # Register your models here.
+
+from orders.models import Order, OrderItem
+
+
+class OrderItemAdmin(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+    verbose_name = 'Товар'
+    verbose_name_plural = 'Товари'
+    readonly_fields = ('price', )
+    form = OrderItemModelForm
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemAdmin]
+    list_display = ['first_name',
+                    'last_name',
+                    'email',
+                    'phone',
+                    'created',
+                    'updated',
+                    'confirmed',
+                    'sent',
+                    'paid',
+                    'declined'
+                    ]
+
+    class Meta:
+        model = Order
+

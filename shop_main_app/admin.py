7fcead50d69c6ccbbf0415116f6c13be135cc226
Igor Laptop <you@example.com>
2023-10-15 13:30:38 +0300

    Added images in list display in django admin for product and category models

--- shop_main_app/admin.py
+++ shop_main_app/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from shop_main_app.models import Category, Measure, Product, ProductImage, User
 from django.contrib.auth.models import Group
 
@@ -23,19 +25,26 @@ class ProductImageAdmin(admin.TabularInline):
 class CategoryAdmin(admin.ModelAdmin):
     # exclude = ('slug',)
     readonly_fields = ('slug', )
+    list_display = ('name', 'images')
 
     class Meta:
         model = Category
+
+    def images(self, obj):
+        return format_html('<img src="{}" style="max-width:100px; max-height:100px"/>'.format(obj.image.url))
 
 
 @admin.register(Product)
 class PostAdmin(admin.ModelAdmin):
     inlines = [ProductImageAdmin]
-    list_display = ['title', 'price', 'quantity', 'availability', 'index']
+    list_display = ['title', 'price', 'quantity', 'availability', 'index', 'images', ]
     readonly_fields = ('slug',)
 
     class Meta:
         model = Product
 
+    def images(self, obj):
+        html = '<a href="{url}" target="_blank"><img style="width: 100px; height: 100px;" src="{url}" /></a>'
+        return format_html(''.join(html.format(url=obj.images.first().image.url)))
 
 
